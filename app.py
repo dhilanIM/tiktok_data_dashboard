28b62053c86684d1cddc71ad59686b40538929d3
dhilanIM <cls_dhilan@hotmail.com>
2025-07-30 16:20:57 -0600

    v0

---
+++ app.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import streamlit as st 
+
+def dashboard():
+    pass
+
+def linear_regression():
+    st.title("Linear Regression Dashboard")
+    st.write("This is a placeholder for the linear regression dashboard.")
+    
+def top_creatives():
+    st.title("Top Creatives tik toks")
+    st.write("This is a placeholder for the top creatives dashboard.")
+    
+def main():
+    pass
